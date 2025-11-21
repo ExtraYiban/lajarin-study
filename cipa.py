9ac6ee64e3445ed286b19adaa21a7044e118b42f
cipa-dev <risal9719@gmail.com>
2025-11-21 10:40:14 +0800

    latihanpa

--- cipa.py
+++ cipa.py
@@ -35,21 +35,23 @@
 
 # mantab 100/100
 
-print("DATA MAHASISWA")
-username = input("masukkan username: ")
-try:
-    umur = int(input("masukkan umur: "))
-    ipk = float(input("masukkan IPK: "))
-except ValueError:
-    print("ERROR: umur & IPK harus angka!")
-    exit()
-status_input = input("apakah mahasiswa aktif? (ya/tidak): ")
-if status_input == "ya" :
-    status = True
-else:
-    False
+# print("DATA MAHASISWA")
+# username = input("masukkan username: ")
+# try:
+#     umur = int(input("masukkan umur: "))
+#     ipk = float(input("masukkan IPK: "))
+# except ValueError:
+#     print("ERROR: umur & IPK harus angka!")
+#     exit()
+# status_input = input("apakah mahasiswa aktif? (ya/tidak): ")
+# if status_input == "ya" :
+#     status = True
+# else:
+#     False
 
-print("\n=== OUTPUT ===")
-print(f"username: {username} | umur: {umur}")
-print(f"IPK: {ipk}, status: {status}")
-print(f"mahasiswa {username} berstatus aktif: {status}")
+# print("\n=== OUTPUT ===")
+# print(f"username: {username} | umur: {umur}")
+# print(f"IPK: {ipk}, status: {status}")
+# print(f"mahasiswa {username} berstatus aktif: {status}")
+
+
