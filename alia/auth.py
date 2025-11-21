379e6773e089f2967d6ef21da1bf8178adaa57f2
Alia <aliafransiska30@gmail.com>
2025-11-21 10:44:21 +0700

    a

--- alia/auth.py
+++ alia/auth.py
@@ -1,14 +1,12 @@
-import os
+
 def login():
-    while True:
-        os.system("cls || clear")
-        username = input("username : ").strip()
-        password = input("password : ")
-        if username == "admin" and password == "admin123":
-            role = "admin"
-        elif username == "staff" and password == "staff123":
-            role = "staff"
-        else:
-            print("SALAHHHHHH")
-            enter = input("tekan enter kalo mau login ulang")
-        # return role
+    username = input("username : ").strip()
+    password = input("password : ")
+    if username == "admin" and password == "admin123":
+        role = "admin"
+    elif username == "staff" and password == "staff123":
+        role = "staff"
+    else:
+        role = "none"
+        print("SALAHHHHHH")
+    return role
